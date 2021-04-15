5243d3aef32c200e320488781815072cd5f42759
Leah Miller <leahmiller1969@gmail.com>
2021-04-15 13:35:57 -0700

    conditional rendering to display weather

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,20 +1,35 @@
-import {useEffect} from 'react';
-
+import { useEffect, useState } from "react";
 
 function Temperature() {
-  const location = "Las Vegas";
-  const weatherAPIKey = "3869c89cd91949e9972175217211504";
-  const weatherURL = "http://api.weatherapi.com/v1/current.json?key=";
-  const fullAPI = weatherURL + weatherAPIKey + "&q=" + location + "&aqi=yes";
+  const [data, setData] = useState(null);
+  const [isReady, setIsReady] = useState(false)
 
   useEffect(() => {
+      const location = "Las Vegas";
+      const weatherAPIKey = "3869c89cd91949e9972175217211504";
+      const weatherURL = "http://api.weatherapi.com/v1/current.json?key=";
+      const fullAPI = weatherURL + weatherAPIKey + "&q=" + location + "&aqi=yes";
     fetch(fullAPI)
-      .then(response => response.json())
-      .then(data => console.log(data))
-  })  
+      .then(res => res.json())
+      .then(data => setData(data))
+      .then(isReady => setIsReady(true))
+  },[])  
 
-  return <p> temperature</p>;
-  //
+  console.log(data);
+  if (!isReady) {
+    return (
+      null
+    )
+  } 
+
+  return (
+    <>
+    <p>{data.location.name}</p>
+    <p>{data.location.region}</p>
+    <p>{data.current.condition.text}</p>
+    </>
+  )
+
 }
 
 export default Temperature;
